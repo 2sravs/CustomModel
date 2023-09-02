c6748e41450c2ec7ead315f0a796a624681680ae
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-02 19:57:27 +0530

    Firstcommit

---
+++ Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace CustomModelBinding.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string WhichClass { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+
+    }
+}
